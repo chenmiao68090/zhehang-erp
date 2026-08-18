acd25746867e1373799936df53ab64624109ffdf
陈苗 <chenmiao@192.168.5.69>
2026-08-19 01:02:04 +0800

    chore: 删除废弃的 project 模块(死代码)
    - modules/project/ 共 26 文件(PmProject/PmTask/PmMilestone/PmDoc/PmTimesheet)
    - 前端已无 /project/* 路由/页面/api 调用,后端无其他模块引用,报表 preset 不依赖 pm_ 表
    - 顺带消除该模块遗留的越权点(旧越权清单里 project/task 的 getInfo/edit 等)
    - 数据库 pm_* 表保留为孤儿表(无害,不删数据)

--- zhehang-erp-server/zhehang-erp-modules/src/main/java/com/zhehang/erp/modules/project/mapper/PmMilestoneMapper.java
+++
@@ -1,9 +0,0 @@
-package com.zhehang.erp.modules.project.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zhehang.erp.modules.project.domain.entity.PmMilestone;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PmMilestoneMapper extends BaseMapper<PmMilestone> {
-}
--- zhehang-erp-server/zhehang-erp-modules/src/main/java/com/zhehang/erp/modules/project/mapper/PmProjectMapper.java
+++
@@ -1,9 +0,0 @@
-package com.zhehang.erp.modules.project.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zhehang.erp.modules.project.domain.entity.PmProject;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PmProjectMapper extends BaseMapper<PmProject> {
-}
